f9b505078f72d6d18ffae50ee05f158d937c64ef
Thibaud Colas <thibaudcolas@gmail.com>
2020-02-26 12:10:23 +0000

    Add missing static URL patterns

--- demo/urls.py
+++ demo/urls.py
@@ -1,6 +1,7 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-urlpatterns = [
+urlpatterns = staticfiles_urlpatterns() + [
     path("storybook/", include("demo.storybook.urls")),
     path("pattern-library/", include("pattern_library.urls")),
 ]
