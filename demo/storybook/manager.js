4742b729336829f2619caa3c14e1ee3938768d90
Thibaud Colas <thibaudcolas@gmail.com>
2020-07-27 10:42:46 +0100

    Upgrade to latest Storybook RC

---
+++ demo/storybook/manager.js
@@ -0,0 +1,8 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+
+import theme from './theme';
+
+addons.setConfig({
+    theme,
+});
---
+++ demo/storybook/theme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+
+    colorPrimary: 'hotpink',
+    colorSecondary: '#0366d6',
+
+    brandTitle: 'Storybook & Django',
+    brandUrl: null,
+});
