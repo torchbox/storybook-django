d1432ea5b02dd64a8cb5372868b1067e39543999
Thibaud Colas <thibaudcolas@gmail.com>
2021-07-10 17:52:45 +0100

    Set up basic Python QA in CI

--- demo/storybook/urls.py
+++ demo/storybook/urls.py
@@ -4,14 +4,14 @@ from django.views.static import serve
 
 from . import views
 
+DOC_ROOT = settings.STORYBOOK_DIR
+
 urlpatterns = [
     path(
         "api/v1/pattern-library/",
         views.RenderPatternView.as_view(),
         name="render_pattern_api",
     ),
-    re_path(
-        r"^$", serve, {"path": "index.html", "document_root": settings.STORYBOOK_DIR}
-    ),
-    re_path(r"^(?P<path>.*)$", serve, {"document_root": settings.STORYBOOK_DIR}),
+    re_path(r"^$", serve, {"path": "index.html", "document_root": DOC_ROOT}),
+    re_path(r"^(?P<path>.*)$", serve, {"document_root": DOC_ROOT}),
 ]
