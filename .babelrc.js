df5baf4c25c71746b5fbcdebcd9f234d4ded18e9
Thibaud Colas <thibaudcolas@gmail.com>
2021-07-10 17:52:21 +0100

    Start adding requirements for Storyshots

--- .babelrc.js
+++ .babelrc.js
@@ -10,7 +10,18 @@ module.exports = {
     ],
     env: {
         test: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            node: 'current',
+                        },
+                    },
+                ],
+                '@babel/preset-react',
+            ],
         },
     },
+    plugins: ['transform-require-context'],
 };
--- demo/static_src/tests/setupTests.js
+++ demo/static_src/tests/setupTests.js
@@ -1,2 +1,5 @@
 // See https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Needed for Storyshots tests loading backend data.
+import 'whatwg-fetch';
