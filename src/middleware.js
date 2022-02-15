3cb247128ed01066f4d8fbb9774b55b32701b357
Thibaud Colas <thibaudcolas@gmail.com>
2022-02-15 11:34:28 +0000

    Switch http-proxy-middleware to be a dependency of the package

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,5 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 // https://github.com/chimurai/http-proxy-middleware/issues/40#issuecomment-249430255
 const restream = (proxyReq, req) => {
     if (req.body) {
@@ -11,17 +13,18 @@ const restream = (proxyReq, req) => {
 /**
  * Initialises a http-proxy-middleware for Storybook to talk to Django without CORS issues.
  * @param {object} options options
- * @param {object} options.proxy http-proxy-middleware module
- * @param {string} options.apiPath e.g. /api/v1/pattern-library/
- * @param {string} options.djangoDomain e.g. http://localhost:8000
+ * @param {string} options.origin where Django runs, for example http://localhost:8000
+ * @param {string} options.apiPath API endpoint to render patterns (/pattern-library/api/v1/render-pattern)
+ * @param {function} options.proxy http-proxy-middleware module
  */
 const createDjangoAPIMiddleware = (options) => {
-    const { proxy, apiPath, djangoDomain } = options;
+    const { proxy, apiPath, origin } = options;
+    const createProxy = proxy || createProxyMiddleware;
     const middleware = (router) => {
         router.use(
             apiPath,
-            proxy({
-                target: djangoDomain,
+            createProxy({
+                target: origin,
                 changeOrigin: true,
                 onProxyReq: restream,
             }),
